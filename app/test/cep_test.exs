2612dc41aba86c5b446aa712ddcfec70ff611093
Breno Panzolini <bpanzolini@gmail.com>
2017-08-13 10:17:33 -0300

    change test order

--- app/test/cep_test.exs
+++ app/test/cep_test.exs
@@ -1,25 +1,18 @@
 defmodule Cep.Test do
   use ExUnit.Case, async: true
  
-  test "deve retornar erro ao informar CEP com menos de 8 caracteres" do
-    cep = "1001000"
-    resultado = Cep.buscar(cep)
-
-    assert {:error, "CEP deve ter 8 caracteres"} == resultado
-  end
-
-  test "deve retornar erro ao informar CEP com mais de 8 caracteres" do
-    cep = "010010000"
-    resultado = Cep.buscar(cep)
-
-    assert {:error, "CEP deve ter 8 caracteres"} == resultado
-  end
-
   test "deve retornar erro ao informar CEP nil" do
     cep = nil
     resultado = Cep.buscar(cep)
 
     assert {:error, "CEP não pode ser nil"} == resultado
+  end
+
+  test "deve retornar erro ao informar CEP que não seja string" do
+    cep = 01001000
+    resultado = Cep.buscar(cep)
+
+    assert {:error, "CEP deve ser String"} == resultado      
   end
 
   test "deve retornar erro ao informar CEP vazio" do
@@ -36,18 +29,25 @@ defmodule Cep.Test do
     assert {:error, "CEP não pode ser vazio"} == resultado
   end
 
+  test "deve retornar erro ao informar CEP com menos de 8 caracteres" do
+    cep = "1001000"
+    resultado = Cep.buscar(cep)
+
+    assert {:error, "CEP deve ter 8 caracteres"} == resultado
+  end
+
+  test "deve retornar erro ao informar CEP com mais de 8 caracteres" do
+    cep = "010010000"
+    resultado = Cep.buscar(cep)
+
+    assert {:error, "CEP deve ter 8 caracteres"} == resultado
+  end
+
   test "deve retornar erro ao informar CEP com caracteres não numéricos" do
     cep = "0100X000"
     resultado = Cep.buscar(cep)
 
     assert {:error, "CEP deve conter apenas números"} == resultado
-  end
-
-  test "deve retornar erro ao informar CEP que não seja string" do
-    cep = 01001000
-    resultado = Cep.buscar(cep)
-
-    assert {:error, "CEP deve ser String"} == resultado      
   end
 
 end
