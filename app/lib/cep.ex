6e444cc4a6e27aa3eded105abacdceca1c8acba4
Breno Panzolini <bpanzolini@gmail.com>
2017-08-13 10:05:32 -0300

    refactor project test

--- app/lib/cep.ex
+++ app/lib/cep.ex
@@ -1,15 +1,16 @@
-defmodule App.Cep do
+defmodule Cep do
 
-  def get_endpoint(cep) do
+  defp get_endpoint(cep) do
     "https://viacep.com.br/ws/#{cep}/json/" 
   end
 
   def buscar(cep) do
     cond do
       is_nil(cep) -> {:error, "CEP não pode ser nil"}
+      !is_bitstring(cep) -> {:error, "CEP deve ser String"}
       String.trim(cep) == "" -> {:error, "CEP não pode ser vazio"}
+      Regex.match?(~r/[^0-9]/, cep) -> {:error, "CEP deve conter apenas números"}
       String.length(cep) < 8 || String.length(cep) > 8 -> {:error, "CEP deve ter 8 caracteres"}
-      !is_integer(cep) -> {:error, "CEP deve conter apenas números"}
     end
   end
 
--- app/test/cep_test.exs
+++ app/test/cep_test.exs
@@ -1,53 +1,53 @@
-defmodule App.Cep.Test do
+defmodule Cep.Test do
   use ExUnit.Case, async: true
  
-  test "deve retornar o endpoint com o CEP informado" do
-    cep = "01001000"
-    endpoint = App.Cep.get_endpoint(cep)
-
-    assert "https://viacep.com.br/ws/#{cep}/json/" == endpoint
-  end
-
   test "deve retornar erro ao informar CEP com menos de 8 caracteres" do
     cep = "1001000"
-    resultado = App.Cep.buscar(cep)
+    resultado = Cep.buscar(cep)
 
     assert {:error, "CEP deve ter 8 caracteres"} == resultado
   end
 
   test "deve retornar erro ao informar CEP com mais de 8 caracteres" do
     cep = "010010000"
-    resultado = App.Cep.buscar(cep)
+    resultado = Cep.buscar(cep)
 
     assert {:error, "CEP deve ter 8 caracteres"} == resultado
   end
 
   test "deve retornar erro ao informar CEP nil" do
     cep = nil
-    resultado = App.Cep.buscar(cep)
+    resultado = Cep.buscar(cep)
 
     assert {:error, "CEP não pode ser nil"} == resultado
   end
 
   test "deve retornar erro ao informar CEP vazio" do
     cep = ""
-    resultado = App.Cep.buscar(cep)
+    resultado = Cep.buscar(cep)
 
     assert {:error, "CEP não pode ser vazio"} == resultado
   end
 
   test "deve retornar erro ao informar CEP apenas com espaços" do
     cep = " "
-    resultado = App.Cep.buscar(cep)
+    resultado = Cep.buscar(cep)
 
     assert {:error, "CEP não pode ser vazio"} == resultado
   end
 
   test "deve retornar erro ao informar CEP com caracteres não numéricos" do
     cep = "0100X000"
-    resultado = App.Cep.buscar(cep)
+    resultado = Cep.buscar(cep)
 
     assert {:error, "CEP deve conter apenas números"} == resultado
+  end
+
+  test "deve retornar erro ao informar CEP que não seja string" do
+    cep = 01001000
+    resultado = Cep.buscar(cep)
+
+    assert {:error, "CEP deve ser String"} == resultado      
   end
 
 end
