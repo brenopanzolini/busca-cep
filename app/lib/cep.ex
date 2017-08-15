955ca62c3cab0d9f1985d434e74f85947a64eff8
Breno Panzolini <bpanzolini@gmail.com>
2017-08-15 20:14:07 -0300

    fetch cep from ViaCEP

--- app/lib/cep.ex
+++ app/lib/cep.ex
@@ -1,16 +1,34 @@
 defmodule Cep do
 
-  defp get_endpoint(cep) do
-    "https://viacep.com.br/ws/#{cep}/json/" 
+  defp fetch_viacep(cep) do
+    "https://viacep.com.br/ws/#{cep}/json/" |> HTTPoison.get |> parser_response
+  end
+
+  defp parser_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    {_, json} = body |> JSON.decode
+
+    if json["erro"] == true do
+      {:error, "CEP não encontrado"}
+    else
+      endereco = %{:cep => json["cep"], :logradouro => json["logradouro"], :complemento => json["complemento"], :bairro => json["bairro"], :localidade => json["localidade"], :uf => json["uf"]}      
+      {:ok, endereco}
+    end
   end
 
   def buscar(cep) do
     cond do
-      is_nil(cep) -> {:error, "CEP não pode ser nil"}
-      !is_bitstring(cep) -> {:error, "CEP deve ser String"}
-      String.trim(cep) == "" -> {:error, "CEP não pode ser vazio"}
-      Regex.match?(~r/[^0-9]/, cep) -> {:error, "CEP deve conter apenas números"}
-      String.length(cep) < 8 || String.length(cep) > 8 -> {:error, "CEP deve ter 8 caracteres"}
+      is_nil(cep) ->
+        {:error, "CEP não pode ser nil"}
+      !is_bitstring(cep) ->
+        {:error, "CEP deve ser String"}
+      String.trim(cep) == "" ->
+        {:error, "CEP não pode ser vazio"}
+      Regex.match?(~r/[^0-9]/, cep) ->
+        {:error, "CEP deve conter apenas números"}
+      String.length(cep) < 8 || String.length(cep) > 8 ->
+        {:error, "CEP deve ter 8 caracteres"}
+      true ->
+        fetch_viacep(cep)
     end
   end
 
